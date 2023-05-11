f815613a0e3102a78ee32f8c023cfd23c097f0c3
Matt Soucoup <msoucoup@codemilltech.com>
2023-05-11 01:02:42 +0000

    Creating the embedding and search sample

--- download-vocab-files.sh
+++ download-vocab-files.sh
@@ -1,2 +1,3 @@
 curl -o merges.txt https://huggingface.co/gpt2/raw/main/merges.txt
 curl -o vocab.json https://huggingface.co/gpt2/raw/main/vocab.json
+curl -o bill_sum_data.csv https://raw.githubusercontent.com/Azure-Samples/Azure-OpenAI-Docs-Samples/main/Samples/Tutorials/Embeddings/data/bill_sum_data.csv
